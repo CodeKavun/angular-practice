8125ad064e5a939a3ebdac50d049d2c1d676fd60
PanDmytryk <dimacapny22@gmail.com>
2024-08-20 20:31:20 +0200

    Make 2 components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooComponent } from './foo/foo.component';
+import { ItemListComponent } from './item-list/item-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooComponent, ItemListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angularPractice';
+  items = ["Pizza", "Bomb", "Candy", "Pudding"]
 }
