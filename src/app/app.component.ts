03a81c7858beef9987c60d715116f5b676eba157
PanDmytryk <dimacapny22@gmail.com>
2024-08-27 20:00:57 +0200

    Make light practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
+import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import { NewsListComponent } from './news-list/news-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NewsListComponent],
+  imports: [RouterOutlet, FormsModule, NgStyle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angularPractice';
+
+  biggestCarInfo: string = 'The biggest car in the world is - \'American Dream\'';
+  biggestPlaneInfo: string = 'The biggest plane in the world is - \'Мрія\'';
+  biggestShipInfo: string = 'The biggest ship in the world is - \'Seawise Giant\' oil tanker';
+
+  displayText: string = 'Empty text';
+
+  updateInfoText(info: string) {
+    this.displayText = info;
+  }
 }
